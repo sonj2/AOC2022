3b6833cfb686816524f5fc8defd6a05baad64fac
Andrew Son <sonj2@rpi.edu>
2022-12-08 17:04:07 -0500

    progress in csharp

--- Problem 2 csharp/Program.cs
+++ Problem 2 csharp/Program.cs
@@ -1,9 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var sr = new StreamReader("TestData.txt");
+var sr = new StreamReader("input.txt");
 string? s = "";
 List<int> values = new List<int>();
+int roundscore = 0;
+int totalscore = 0;
 while ((s = sr.ReadLine()) != null)
 {
-    
+    switch (s[0])
+    {
+        case 'A': // Rock
+            switch (s[2])
+            {
+                case 'Y': // Paper
+                    roundscore += 8; // 6 + 2
+                    break;
+                case 'X': // Rock
+                    roundscore += 4; // 3 + 1
+                    break;
+                case 'Z': // Scissors
+                    roundscore += 3; // 0 + 3
+                    break;
+            }
+            totalscore += roundscore;
+            break;
+        case 'B': // Paper
+            switch (s[2])
+            {
+                case 'Y': // Paper
+                    roundscore += 5; // 3 + 2
+                    break;
+                case 'X': // Rock
+                    roundscore += 1; // 0 + 1
+                    break;
+                case 'Z': // Scissors
+                    roundscore += 9; // 6 + 3
+                    break;
+            }
+            totalscore += roundscore;
+            break;
+        case 'C': // Scissors
+            switch (s[2])
+            {
+                case 'Y': // Paper
+                    roundscore += 2; // 0 + 2
+                    break;
+                case 'X': // Rock
+                    roundscore += 7; // 6 + 1
+                    break;
+                case 'Z': // Scissors
+                    roundscore += 6; // 3 + 3
+                    break;
+            }
+            totalscore += roundscore;
+            break;
+    }
+    roundscore = 0;
 }
+Console.WriteLine(totalscore);
--- Problem2_Rust/problem2_rust/src/main.rs
+++ Problem2_Rust/problem2_rust/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs;
 
 fn main() {
-    let data = fs::read_to_string("TestData.txt");
+    let data = fs::read_to_string("input.txt");
     let binding = data.unwrap();
     let lines = binding.lines();
-    let mut vector = Vec::<i32>::new();
+    let mut roundscore = 0;
+    let mut finalscore = 0;
     for line in lines {
+        
     }
+    println!("Final score: {}", finalscore);
 }
