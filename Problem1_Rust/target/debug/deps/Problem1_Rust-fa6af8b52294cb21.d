f7feb22cccee98e4b309df5aff0915392c17dc55
Andrew Son <sonj2@rpi.edu>
2022-12-08 05:28:26 -0500

    cleaning things up

--- Problem1_Rust/target/debug/deps/Problem1_Rust-fa6af8b52294cb21.d
+++
@@ -1,5 +0,0 @@
-/Users/momiji/Documents/Github Projects/AOC2022/Problem1_Rust/target/debug/deps/Problem1_Rust-fa6af8b52294cb21.rmeta: src/main.rs
-
-/Users/momiji/Documents/Github Projects/AOC2022/Problem1_Rust/target/debug/deps/Problem1_Rust-fa6af8b52294cb21.d: src/main.rs
-
-src/main.rs:
--- Problem1_Rust/target/debug/deps/problem1_rust-88ea3ca618725e51.d
+++
@@ -1,5 +0,0 @@
-/Users/momiji/Documents/Github Projects/AOC2022/Problem1_Rust/target/debug/deps/problem1_rust-88ea3ca618725e51.rmeta: src/main.rs
-
-/Users/momiji/Documents/Github Projects/AOC2022/Problem1_Rust/target/debug/deps/problem1_rust-88ea3ca618725e51.d: src/main.rs
-
-src/main.rs:
